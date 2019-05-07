8b847c6a6bdab468eb67c98d9ee5ecf7bf4ba4a5
Arthur I <sovietbulldozer@gmail.com>
2019-05-07 12:13:32 -0400

    Updates

---
+++ precache-manifest.f63f01b4a9a15560397054c6ee30dee4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1d703b9b3d7ecb46674",
+    "url": "/dimboxes/static/css/main.8a369037.chunk.css"
+  },
+  {
+    "revision": "c1d703b9b3d7ecb46674",
+    "url": "/dimboxes/static/js/main.c1d703b9.chunk.js"
+  },
+  {
+    "revision": "207e5d056e3105dcde38",
+    "url": "/dimboxes/static/js/1.207e5d05.chunk.js"
+  },
+  {
+    "revision": "d15e310233528222c17f",
+    "url": "/dimboxes/static/js/runtime~main.d15e3102.js"
+  },
+  {
+    "revision": "06f0f3bf0d94e9410d6cee1aab011079",
+    "url": "/dimboxes/index.html"
+  }
+];
